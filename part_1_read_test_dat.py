ab5d89229bbfbbc29ff846eb0f62a336540f7405
idolish7 <122846207+idolish7@users.noreply.github.com>
2024-02-13 10:34:01 -0500

    Added test txt file, screenshot of running

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -3,6 +3,7 @@ import cc_dat_utils
 #Part 1
 input_dat_file = "data/pfgd_test.dat"
 
-
 #Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
 #print the resulting data
+data = cc_dat_utils.make_cc_level_pack_from_dat(input_dat_file)
+print(data)
